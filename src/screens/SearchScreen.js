f3bda608b1acccb9637b12e820c1d9de4e3e06b5
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-04-19 16:52:41 +0530

    Search screen re-styled

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { API } from 'aws-amplify';
-import { Container, Header, Item, Input, Icon } from 'native-base';
-import { StyleSheet, FlatList, Text } from 'react-native';
+import { Header, SearchBar } from 'react-native-elements';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { searchQuestions, searchAnswers } from '../graphql/queries';
 import QuestionComponent from '../components/QuestionComponent';
 import AnswerComponent from '../components/AnswerComponent';
+import FloatingAskQuestionButton from '../components/FloatingAskQuestionButton';
 
 const SearchScreen = ({ navigation }) => {
   const [input, setInput] = useState('');
@@ -43,37 +44,35 @@ const SearchScreen = ({ navigation }) => {
     );
 
   return (
-    <Container style={styles.container}>
+    <View style={styles.container}>
       <Header
-        androidStatusBarColor="#fff8f5"
-        iosBarStyle="dark-content"
-        transparent
-        searchBar
-        style={styles.header}>
-        <Item regular style={styles.item}>
-          <Icon name="search" type="FontAwesome" style={{ color: '#8f8c8b' }} />
-          <Input
-            placeholder="Search..."
-            placeholderTextColor="#8f8c8b"
-            style={styles.input}
-            onChangeText={(text) => searchItems(text)}
-            value={input}
-          />
-        </Item>
-      </Header>
+        statusBarProps={{ barStyle: 'dark-content' }}
+        style={styles.header}
+        backgroundColor="white"
+      />
+      <SearchBar
+        placeholder="Try 'How a ...'"
+        lightTheme
+        containerStyle={styles.Search}
+        inputContainerStyle={styles.SearchInput}
+        onChangeText={(text) => searchItems(text)}
+        value={input}
+      />
       <FlatList
         data={results}
         renderItem={RenderItem}
         keyExtractor={(item) => item.id}
-        style={styles.FlatList}
       />
-    </Container>
+      {/** Floating Ask Question Button */}
+      <FloatingAskQuestionButton navigation={navigation} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff8f5',
+    flex: 1,
+    backgroundColor: 'white',
   },
   item: {
     backgroundColor: '#faf4f2',
@@ -81,9 +80,14 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     borderRadius: 7,
   },
-  input: {
-    fontSize: 15,
-    paddingLeft: 10,
+  Search: {
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  SearchInput: {
+    backgroundColor: 'white',
+    elevation: 5,
+    borderRadius: 10,
   },
 });
 
