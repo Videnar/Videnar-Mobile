59b90dd9a391a57d018b872c9a2887206ecb579d
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-04-22 13:21:21 +0530

    education selection basic

--- src/components/EducationSelectionComponent.js
+++ src/components/EducationSelectionComponent.js
@@ -1,13 +1,73 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import React, { useState } from 'react';
+import { Pressable, ScrollView, StyleSheet, View } from 'react-native';
+import { Text, Overlay, Button, Header, ListItem } from 'react-native-elements';
+import { educations } from '../utilities/constants/education';
 
 const EducationSelectionComponent = () => {
+  const [showEduOverlay, setShowEduOverlay] = useState(false);
+  const [selectText, setSelectText] = useState('Select');
+
+  const onPressHandler = (education) => {
+    setSelectText(education);
+    setShowEduOverlay(false);
+  };
+
+  const RenderItem = educations.map((item) => (
+    <ListItem
+      key={item.level}
+      containerStyle={styles.list}
+      onPress={() => onPressHandler(item.level)}>
+      <ListItem.Title>{item.level}</ListItem.Title>
+      <ListItem.Chevron />
+    </ListItem>
+  ));
   return (
     <View>
       <Text>Education</Text>
+      <Pressable
+        onPress={() => setShowEduOverlay(true)}
+        style={styles.selector}>
+        <Text>{selectText}</Text>
+      </Pressable>
+      <Overlay
+        isVisible={showEduOverlay}
+        onBackdropPress={() => setShowEduOverlay(false)}>
+        <Header
+          statusBarProps={{
+            barStyle: 'dark-content',
+            backgroundColor: 'white',
+          }}
+          leftComponent={{
+            icon: 'arrow-back',
+            onPress: () => setShowEduOverlay(false),
+            color: 'white',
+          }}
+          centerComponent={{
+            text: 'Select Your Education',
+            style: styles.headerText,
+          }}
+          backgroundColor="purple"
+        />
+        <ScrollView>{RenderItem}</ScrollView>
+      </Overlay>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  selector: {
+    width: 150,
+    height: 30,
+    backgroundColor: 'yellow',
+  },
+  list: {
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export default EducationSelectionComponent;
