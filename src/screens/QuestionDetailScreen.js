81f912dd7cc341d40893b8261322ba5166389543
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-05-12 15:03:32 +0530

    footers added on question and answer lists

--- src/screens/QuestionDetailScreen.js
+++ src/screens/QuestionDetailScreen.js
@@ -91,6 +91,9 @@ const QuestionDetailScreen = ({ navigation, route }) => {
             <Text> Loading... </Text>
           </View>
         )}
+        <View style={styles.lastItem}>
+          <Text>No More Answers to Show</Text>
+        </View>
       </ScrollView>
       <FAB
         title="Answer"
@@ -122,6 +125,12 @@ const styles = StyleSheet.create({
     height: HEIGHT,
     backgroundColor: 'white',
     alignItems: 'center',
+  },
+  lastItem: {
+    height: 130,
+    alignContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 30,
   },
 });
 
