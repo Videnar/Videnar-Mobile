7ee1a0866e287ae9ce0d0b0cd3315b46dd7d876a
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-07-03 13:58:16 +0530

    toast message added for vote

--- src/components/UpVoteDownVoteComponent.js
+++ src/components/UpVoteDownVoteComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Icon, Text, Tooltip } from 'react-native-elements';
+import { Icon, Text } from 'react-native-elements';
+import Toast from 'react-native-toast-message';
 import { DEEP_GREEN, GREY } from '../assets/colors/colors';
 
 const UpVoteDownVoteComponent = ({
@@ -92,6 +93,18 @@ const UpVoteDownVoteComponent = ({
     addUpvoteData(voteType);
   };
 
+  const disabledVoteHandler = () => {
+    Toast.show({
+      type: 'info',
+      position: 'bottom',
+      text1: "Can't vote your Question/Answer 🤐",
+      visibilityTime: 2000,
+      autoHide: true,
+      topOffset: 40,
+      bottomOffset: 80,
+    });
+  };
+
   return (
     <>
       <View style={styles.upvoteContainer}>
@@ -120,29 +133,25 @@ const UpVoteDownVoteComponent = ({
         </View>
       ) : (
         // Disabled vote
-        <Tooltip
-          popover={<Text>Can't Vote Your Question / Answer 🤐</Text>}
-          containerStyle={styles.disabled}
-          backgroundColor="white"
-          overlayColor="transparent"
-          width={270}>
-          <View style={styles.vote}>
-            <Icon
-              name="forward"
-              type="material"
-              size={28}
-              color={voteColor.upVote}
-              containerStyle={styles.upVote}
-            />
-            <Icon
-              name="forward"
-              type="material"
-              size={28}
-              color={voteColor.downVote}
-              containerStyle={styles.downVote}
-            />
-          </View>
-        </Tooltip>
+
+        <View style={styles.vote}>
+          <Icon
+            name="forward"
+            type="material"
+            size={28}
+            color={voteColor.upVote}
+            containerStyle={styles.upVote}
+            onPress={disabledVoteHandler}
+          />
+          <Icon
+            name="forward"
+            type="material"
+            size={28}
+            color={voteColor.downVote}
+            containerStyle={styles.downVote}
+            onPress={disabledVoteHandler}
+          />
+        </View>
       )}
     </>
   );
